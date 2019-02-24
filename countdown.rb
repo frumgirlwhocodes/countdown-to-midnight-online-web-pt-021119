3f960cd4977d526ef86b3366519288580dcc1899
lieby weintraub <liebahinda@gmail.com>
2019-02-24 21:43:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,17 +1,19 @@
 #write your code here
 
 def countdown(int)
-  int=10 
-  while int > 0 
+  int= 10 
+  while int > 0
   puts "#{int} SECOND(S)!"
   int -= 1
 end 
 "HAPPY NEW YEAR!"
 end
 def countdown_with_sleep(int)
+  int= 10
   while int > 0
     puts "#{int} SECOND(S)!"
     int -= 1
     sleep(1)
   end
+  "HAPPY NEW YEAR"
 end 
